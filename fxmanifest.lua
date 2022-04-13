0e7e2620e93a3fc80808266e8919e397b247e086
5m1Ly <sm1lyprogrammer@gmail.com>
2022-04-13 12:46:30 +0200

    updated images and resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Sm1Ly'
+description 'A resource for optimizing your server its data traffic'
+version '1.6.0'
+stay_up_to_date 'true'
+
+server_scripts {
+
+	-- load wrapper files
+	"server/lib/**/*.lua",
+
+	-- loading extra content
+	"server/src/*.lua"
+
+}
