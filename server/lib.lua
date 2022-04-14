153604289d446c77a00ef4c4400ffc44fa68c65d
5m1Ly <sm1lyprogrammer@gmail.com>
2022-04-14 09:23:48 +0200

    added auto json en/de code function

--- server/lib.lua
+++ server/lib.lua
@@ -3,6 +3,9 @@ SeaQL = {}
 -- meta methods
 SeaQL.__index = SeaQL
 SeaQL.__tostring = Utils.tostring
+SeaQL.__call = function(self, query, data, callback)
+	
+end
 
 SeaQL = setmetatable({
 	info = Info.set("0.0.4"),
